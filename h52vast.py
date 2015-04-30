a1454cb7f83afbc8971ac2d7eb8ae053daa164cd
jingpengwu <jingpeng.wu@gmail.com>
2015-04-30 11:59:46 -0400

    update

--- h52vast.py
+++ h52vast.py
@@ -7,11 +7,12 @@ sudo apt-get install python-numpy python-h5py python-tifffile
 """
 #%% parameters
 import numpy as np
-
+# the file directory
 Dir = '/usr/people/bds2/seungmount/Omni/TracerTasks/Piriform_cortex_Corrections/Chunk_07/Chunk_07_EM/'
 filename ='chunk_07_em'
 tifname = 'chunk_07_em.tif'
 
+# whether the file is gray image
 isgray = True
 
 #%% read the data
@@ -25,11 +26,11 @@ if isgray:
 	outvol = outvol.astype('uint8')
 else:
 	vol = np.asarray(invol* ((2**24-1)/float(invol.max())), dtype='uint32') 
-	rgb = np.zeros( np.append(np.array( invol.shape ), 3), dtype='uint8')
+	outvol = np.zeros( np.append(np.array( invol.shape ), 3), dtype='uint8')
 
-	rgb[:,:,:, 0] = np.remainder( invol, 256)
-	rgb[:,:,:, 1] = np.remainder( invol, 25536) / 256
-	rgb[:,:,:, 2] = invol / 25536
+	outvol[:,:,:, 0] = np.remainder( invol, 256)
+	outvol[:,:,:, 1] = np.remainder( invol, 25536) / 256
+	outvol[:,:,:, 2] = invol / 25536
 
 #%% write as tiff
 import tifffile
